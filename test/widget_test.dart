293cc5f3cf7e31dcb7466d732b2e99013978670d
KalpaWishwajith <kalpawishwajith00000@gmail.com>
2024-11-30 10:12:19 +0530

    test-changed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:hello_flutter_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Hello_());
+    await tester.pumpWidget(const JokeApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
